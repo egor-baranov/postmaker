4b5d5f34431bd01c31c32a5ab48709557ec9a8df
kepler88d <enterskylife@gmail.com>
2023-03-29 22:18:26 +0300

    dynamic menu position

--- components/LexicalEditor.tsx
+++ components/LexicalEditor.tsx
@@ -1,13 +1,19 @@
-import { LexicalComposer } from '@lexical/react/LexicalComposer';
-import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
-import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import {LexicalComposer} from '@lexical/react/LexicalComposer';
+import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
+import {ContentEditable} from '@lexical/react/LexicalContentEditable';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
-import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
-import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
-import { mergeRegister } from '@lexical/utils';
+import {OnChangePlugin} from '@lexical/react/LexicalOnChangePlugin';
+import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
+import {mergeRegister} from '@lexical/utils';
 // @ts-ignore
 import Toolbar from './plugins/ToolbarPlugin';
+import {css} from '@emotion/css'
+import {SELECTION_CHANGE_COMMAND} from 'lexical';
 import {$getRoot, $getSelection} from "lexical";
+import {useLexicalComposerContext} from "@lexical/react/LexicalComposerContext";
+import {selector} from "postcss-selector-parser";
+import {SetStateAction, useCallback, useRef, useState} from "react";
+import {useTextSelection} from "./utils/text_selection";
 
 
 type LexicalEditorProps = {
@@ -24,28 +30,49 @@ function onChange(state: any) {
 }
 
 
-export function LexicalEditor(props: LexicalEditorProps) {
+export const LexicalEditor = (props: LexicalEditorProps) => {
+    const [target, setTarget] = useState<HTMLElement>()
+
+    const ref = useCallback((el: SetStateAction<HTMLElement | undefined> | null) => {
+        if (el != null) {
+            setTarget(el)
+        } else {
+            setTarget(undefined)
+        }
+    }, [])
+
+    const {clientRect, isCollapsed} = useTextSelection(target)
+
+
     return (
         <LexicalComposer initialConfig={props.config}>
             <div className="editor-container">
                 <div className="editor-inner">
-                    <Toolbar />
-                    <RichTextPlugin
-                        contentEditable={<ContentEditable />}
-                        placeholder={<Placeholder />}
-                        ErrorBoundary={LexicalErrorBoundary}
-                    />
+                    {clientRect != null && !isCollapsed ?
+                        <div className={css`
+                          position: absolute;
+                          left: ${clientRect!!.left + clientRect!!.width / 2 - 108}px;
+                          top: ${clientRect!!.y - 210}px;
+                        `}><Toolbar/></div> : <div/>}
+
+                    <div ref={ref}>
+                        <RichTextPlugin
+                            contentEditable={<ContentEditable/>}
+                            placeholder={<Placeholder/>}
+                            ErrorBoundary={LexicalErrorBoundary}
+                        />
+                    </div>
                 </div>
             </div>
-            <OnChangePlugin onChange={onChange} />
-            <HistoryPlugin />
+            <OnChangePlugin onChange={onChange}/>
+            <HistoryPlugin/>
         </LexicalComposer>
     );
 }
 
 const Placeholder = () => {
     return (
-        <div className="absolute top-[1.125rem] left-[1.125rem] opacity-50 text-3xl font-light">
+        <div className="absolute top-0 m-4 opacity-50 text-xl font-light">
             Content
         </div>
     );
@@ -63,7 +90,7 @@ export function Editor() {
                 config={{
                     namespace: 'lexical-editor',
                     theme: {
-                        root: 'p-4 text-3xl font-light border-slate-500 bg-gray-50 border-0 rounded h-full min-h-[200px] focus:outline-none rounded-2xl',
+                        root: 'p-4 text-2xl font-light border-slate-500 bg-gray-50 border-0 rounded h-full min-h-[200px] focus:outline-none rounded-2xl',
                         link: 'cursor-pointer',
                         text: {
                             bold: 'font-semibold',
